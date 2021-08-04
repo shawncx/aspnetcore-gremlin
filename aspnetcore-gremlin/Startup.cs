763593170a47330e3dfafce894fe786c2d6dec42
Xiao Chen <xichen@microsoft.com>
2021-08-04 09:36:04 +0800

    add listkeyurl, hostname and port env

--- aspnetcore-gremlin/Startup.cs
+++ aspnetcore-gremlin/Startup.cs
@@ -19,8 +19,6 @@ namespace aspnetcore_gremlin
 {
     public class Startup
     {
-        private const string GremlinAccountEndpointConnectionStringKey = "AccountEndpoint";
-        private const string GremlinPasswordConnectionStringKey = "AccountKey";
 
         public Startup(IConfiguration configuration)
         {
@@ -67,41 +65,22 @@ namespace aspnetcore_gremlin
 
         private static CosmosDbService InitializeCosmosClientInstance()
         {
-
-            string subscriptionId = "937bc588-a144-4083-8612-5f9ffbbddb14";
-            string resourceGroupName = "servicelinker-test-win-group";
-            string accountName = "servicelinker-gremlin-cosmos";
-            string databaseName = "coreDB";
-            string graphName = "MyItem";
-
-            string resourceEndpoint = Environment.GetEnvironmentVariable("RESOURCECONNECTOR_TESTWEBAPPUSERASSIGNEDIDENTITYCONNECTIONSUCCEEDED_RESOURCEENDPOINT");
             string scope = Environment.GetEnvironmentVariable("RESOURCECONNECTOR_TESTWEBAPPUSERASSIGNEDIDENTITYCONNECTIONSUCCEEDED_SCOPE");
             string clientId = Environment.GetEnvironmentVariable("RESOURCECONNECTOR_TESTWEBAPPUSERASSIGNEDIDENTITYCONNECTIONSUCCEEDED_CLIENTID");
+            string username = Environment.GetEnvironmentVariable("RESOURCECONNECTOR_TESTWEBAPPUSERASSIGNEDIDENTITYCONNECTIONSUCCEEDED_USERNAME");
+            string listKeyUrl = Environment.GetEnvironmentVariable("RESOURCECONNECTOR_TESTWEBAPPUSERASSIGNEDIDENTITYCONNECTIONSUCCEEDED_LISTKEYURL");
 
             string accessToken = GetAccessTokenByMsIdentity(scope, clientId);
 
-            string endpoint = $"https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.DocumentDB/databaseAccounts/{accountName}/listConnectionStrings?api-version=2019-12-12";
             HttpClient httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
 
-            HttpResponseMessage result = httpClient.PostAsync(endpoint, new StringContent("")).Result;
-            DatabaseAccountListConnectionStringsResult connStrResult = result.Content.ReadAsAsync<DatabaseAccountListConnectionStringsResult>().Result;
+            HttpResponseMessage result = httpClient.PostAsync(listKeyUrl, new StringContent("")).Result;
+            DatabaseAccountListKeysResult connStrResult = result.Content.ReadAsAsync<DatabaseAccountListKeysResult>().Result;
 
-            string connectionString = null;
-            foreach (DatabaseAccountConnectionString connStr in connStrResult.ConnectionStrings)
-            {
-                if (connStr.Description.Contains("Primary") && connStr.Description.Contains("Gremlin"))
-                {
-                    connectionString = connStr.ConnectionString;
-                }
-            }
-
-            IDictionary<string, string> connStrDict = ParseConnectionString(connectionString);
-            string dbEndpoint = connStrDict[GremlinAccountEndpointConnectionStringKey];
-            string hostname = ParseGremlinHostNameFromEndpoint(dbEndpoint);
-            int port = int.Parse(ParseGremlinPortFromEndpoint(dbEndpoint));
-            string username = $"/dbs/{databaseName}/colls/{graphName}";
-            string password = connStrDict[GremlinPasswordConnectionStringKey];
+            string password = connStrResult.PrimaryMasterKey;
+            string hostname = Environment.GetEnvironmentVariable("RESOURCECONNECTOR_TESTWEBAPPUSERASSIGNEDIDENTITYCONNECTIONSUCCEEDED_HOSTNAME");
+            int port = int.Parse(Environment.GetEnvironmentVariable("RESOURCECONNECTOR_TESTWEBAPPUSERASSIGNEDIDENTITYCONNECTIONSUCCEEDED_PORT"));
 
             CosmosDbService cosmosDbService = new CosmosDbService(
                 hostname,
@@ -119,61 +98,5 @@ namespace aspnetcore_gremlin
             return token.Token;
         }
 
-        private static IDictionary<string, string> ParseConnectionString(string connectionString)
-        {
-            // connection string is in format: HostName={hostname};Username={username};Password={password};Port={port}
-            IDictionary<string, string> dict = new Dictionary<string, string>();
-            foreach (string seg in connectionString.Split(";"))
-            {
-                int index = seg.IndexOf("=");
-                if (index < 0)
-                {
-                    continue;
-                }
-                string key = seg.Substring(0, index);
-                string value = seg.Substring(index + 1);
-                dict.Add(key, value);
-            }
-            return dict;
-        }
-
-        private static string ParseGremlinHostNameFromEndpoint(string endpoint)
-        {
-            // endpoint is like: https://servicelinker-gremlin-cosmos.documents.azure.com:443/
-            string tempString = endpoint;
-            int colonIndex = tempString.LastIndexOf(":");
-            tempString = tempString.Substring(0, colonIndex);
-            int slashIndex = tempString.LastIndexOf("/");
-            tempString = tempString.Substring(slashIndex + 1);
-            string[] segs = tempString.Split(".");
-            string hostname = "";
-            foreach (string seg in segs)
-            {
-                // Cosmos host name is like servicelinker-gremlin-cosmos.documents.azure.com
-                // Gremlin host name is like servicelinker-gremlin-cosmos.gremlin.cosmos.azure.com
-                // Change documents to gremlin.cosmos
-                if (string.Equals(seg, "documents"))
-                {
-                    hostname += "gremlin.cosmos.";
-                }
-                else
-                {
-                    hostname += seg + ".";
-                }
-            }
-            return hostname.Remove(hostname.Length - 1);
-        }
-
-        private static string ParseGremlinPortFromEndpoint(string endpoint)
-        {
-            // endpoint is like: https://servicelinker-gremlin-cosmos.documents.azure.com:443/
-            int index = endpoint.LastIndexOf(":");
-            string port = endpoint.Substring(index + 1);
-            while (port.EndsWith("/"))
-            {
-                port = port.Remove(port.Length - 1);
-            }
-            return port;
-        }
     }
 }
